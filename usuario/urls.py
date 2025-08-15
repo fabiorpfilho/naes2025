d6afdfb62e7b6d1054410e3b2c51954076cc6e33
fabiorpfilho <fabio.filho2002@hotmail.com>
2025-08-14 21:25:36 -0300

    Configurando usuario

---
+++ usuario/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from .views import CadastroUsuarioView
+
+urlpatterns = [
+    
+    path("login/", LoginView.as_view(
+        template_name = "cadastros/form.html",
+        extra_context={"titulo": "Autenticação"}), name="login"),
+    
+    path("logout/", LogoutView.as_view(), name="logout"),
+    
+    path("alterar-senha/", PasswordChangeView.as_view(
+        template_name="cadastros/form.html",
+        extra_context={"titulo": "Alterar minha senha"}), name="alterar-senha"),
+
+    
+    path("registrar/", CadastroUsuarioView.as_view(), name="registrar"),
+
+]
