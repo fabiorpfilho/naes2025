0d176514e7aed46fe516b7895e22295807041df7
fabiorpfilho <fabio.filho2002@hotmail.com>
2025-08-21 20:54:50 -0300

    Adicionando queryselect para estatísitca na tela inicial

--- paginasweb/views.py
+++ paginasweb/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Sum
 
 # View que apenas renderiza uma página Web
 from django.views.generic import TemplateView
+
+from cadastros.models import Produto, Pedido
+
 
 # Create your views here.
 
@@ -12,7 +16,13 @@ class PaginaInicial(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        #Enviando uma string básica
         context["nome"] = "Fabio Roberto"
+        #Enviar uma lista de objetos
+        context["ultimos_produtos"] = Produto.objects.all().order_by("nome")[:7]
+        context["qtde_produto"] = Produto.objects.all().count()
+        context["qtde_pedido"] = Pedido.objects.all().count()
+        context["vr_total"] = Pedido.objects.aggregate(Sum("total"))["total__sum"]
         return context
 
 
