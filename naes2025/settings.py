327d441c21a272e9051611307640a2e9f8dadc68
fabiorpfilho <fabio.filho2002@hotmail.com>
2025-09-18 21:17:53 -0300

    Configurando o google cloud

--- naes2025/settings.py
+++ naes2025/settings.py
@@ -85,24 +85,24 @@ WSGI_APPLICATION = "naes2025.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres.qxtfinzorrtufawivvkj",
-#         "PASSWORD": "san01xlz",
-#         "HOST": "aws-0-sa-east-1.pooler.supabase.com",
-#         "PORT": "6543",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.qxtfinzorrtufawivvkj",
+        "PASSWORD": "san01xlz",
+        "HOST": "aws-0-sa-east-1.pooler.supabase.com",
+        "PORT": "6543",
+    }
+}
 
 
 # Password validation
--- naes2025/urls.py
+++ naes2025/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+from debug_toolbar
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +25,6 @@ urlpatterns = [
     path("", include("paginasweb.urls")),
     path("cadastro/", include("cadastros.urls")),
     path("", include("usuario.urls")),
-] + debug_toolbar_urls()
+    
+    path('_debug_/', include(debug_toolbar.urls))
+]
